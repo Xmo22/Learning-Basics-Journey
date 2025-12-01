e2aef683f82679445bd5ee8b400e404676dce36f
Mohamed Emad <xmoemad.10@gmail.com>
2025-12-01 16:02:11 +0200

    Add Linked List Stack implementation & update gitignore

---
+++ 2.Data Structure/StackUsingLinkedList.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+using namespace std;
+template<class t>
+
+
+class Stack{
+
+    struct node{
+        t val;
+        node *next;
+
+    };
+    
+    private: 
+    node *top;
+
+    public:
+    Stack(){
+        top = NULL;
+    }
+
+    void push(t element){
+        node *push_ptr = new node;
+        push_ptr->val = element;
+        push_ptr ->next = top;
+        top = push_ptr;
+    }
+
+    bool isEmpty(){
+        return top == NULL  ;
+    }
+
+    void pop (){
+        if(isEmpty()){
+            cout << "Stack is Empty\n";
+        }
+        else{
+            node *temp_pop = top;
+            top = top->next;
+            delete temp_pop;
+        }
+}
+
+
+
+        void pop (t &element){
+        if(isEmpty()){
+            cout << "Stack is Empty\n";
+        }
+        else{
+            element = top->val;
+            cout << "Popped Element is: " << element <<endl;
+            node *temp_pop = top;
+            top = top->next;
+            delete temp_pop;
+        }
+}
+        void get_top (t &element){
+        if(isEmpty()){
+            cout << "Stack is Empty\n";
+        }
+        else{
+            element = top->val;
+            cout << "Top Element is: " << element <<endl;
+        }
+}
+
+    void display_stack(){
+        cout << "[ ";
+         node *curr = top;
+        while (curr != NULL){
+        cout << curr -> val << " ";
+        curr = curr->next; 
+        }
+        cout << " ]";
+    }
+
+
+
+};
+
+
+int main(){
+    Stack <int> s1;
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    s1.display_stack();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+// template <class t>
+
+
+// class Stack{
+    
+//     struct node{
+//         t val;
+//         node *next;
+//     };
+
+//     private:
+//     node *top ;
+
+//     public:
+
+//     Stack(){
+//         top = NULL;
+//     }
+
+//     void push(t element){
+//         node *push_ptr = new node;
+//         push_ptr -> val = element;
+//         push_ptr -> next = top;
+//         top = push_ptr;
+//     }
+
+//     void pop(){
+
+//         if(top == NULL){
+//             cout << "Stack is already empty\n"; 
+//         }
+//         else{
+
+        
+//         node *tempptr = top;
+//         top = top->next;
+//         delete tempptr;
+//     }
+//     }
+
+//     void pop(t &popped_element){
+//         if(top == NULL){
+//         cout <<"Stack is already empty\n"; 
+//         }
+//         else{
+//         popped_element = top->val;
+//         cout << "Popped element: " << popped_element << endl;
+//         node *tempptr = top;
+//         top = top->next;
+//         delete tempptr;
+//     }
+//     }
+
+//     void get_top(t &top_element){
+//         if(top == NULL){
+//         cout << "Stack is already empty\n"; 
+//         }
+//         else{
+//             top_element = top->val;
+//             cout << "Top is: " << top_element << endl;
+
+//         }
+//     }
+
+//     void display_stack(){
+//         node *curr = top;
+//         cout << "[ " ;
+//         while(curr != NULL){
+//             cout << curr -> val << " ";
+//             curr = curr -> next;
+//         }
+//         cout << " ]" << "\n";
+//     }
+
+
+//     };
