947f2a0dbfa507221ff956637e021725f7bf5349
Mohamed Emad <xmoemad.10@gmail.com>
2025-12-21 16:51:12 +0200

    recursion

---
+++ 1.Problem Solving/1.CodeWars8KYUCPP/9.Recursion.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int factorial(int n){
+    if( n==0 || n == 1){
+        return 1;
+    }
+    else{
+        return n * factorial(n-1);
+    }
+}
+
+int fibonacci(int n){
+    if (n==0 || n==1){
+        return n;
+
+    }
+    else{
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+int main(){
+    cout <<"Factorial= " << factorial(7) << endl;
+    cout << "Fibonacci number = " << fibonacci(9) << endl;
+
+}
