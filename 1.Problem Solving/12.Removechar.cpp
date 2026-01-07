569754c93a2574996f93467e8460b6f204da3834
Mohamed Emad <xmoemad.10@gmail.com>
2026-01-08 00:44:39 +0200

    Remove ExclamationMarks

---
+++ 1.Problem Solving/12.Removechar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+string removeExclamationMarks(string str){
+   string clean = "";
+  for (unsigned int i = 0 ; i < str.length() ; i ++){
+    if(str[i] == '!'){
+      continue;
+    }
+    clean += str[i];
+  }
+  return clean;
+}
+
+int main(){
+    cout << removeExclamationMarks("Hel!lo! !world!") << "\n";
+}
